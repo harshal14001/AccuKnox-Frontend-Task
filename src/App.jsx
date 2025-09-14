bf4c0a0ed49be59c81560bc31fc3f54514dc2fe8
harshal14001 <harshalargade2001@gmail.com>
2025-09-14 16:25:26 +0530

    before db cat

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Dashboard from "./components/CNAPP-Dashboard/Dashboard";
 import Nav from "./components/Navbar/Nav" ;
+import Opdb from "./components/Operational-Dashb/Opdb";
 
 const App = () => {
   return (
     <div>
       <Nav/>
       <Dashboard/>
+      <Opdb/>
     </div>
   );
 }
---
+++ src/components/Operational-Dashb/Opdb.jsx
@@ -0,0 +1,15 @@
+import './Opdb.css';
+
+const Opdb = () => {
+  return (
+    <section className='opdb-main'>
+        <div className='cspm'>CNAPP Executive Dashboard
+            
+        </div>
+        
+
+    </section>
+  )
+}
+
+export default Opdb;
